b8ed73c66d047aaefae0eb125e015d2242cf5e5f
DextrophysicalMegademolisher <lebilebi06@gmail.com>
2026-02-28 01:15:20 -0200

    Create powerial.py

---
+++ powerial.py
@@ -0,0 +1,25 @@
+import sympy
+def powerial(n, d):
+    if n>1:
+        return n^powerial(n-1, d)
+    elif n<0:
+        return powerial(n+1, d)^(1/(n+1))
+    else:
+        x=sympy.symbols('x')
+        co=sympy.symbols(f'a0:{d+1}')
+        f=sum(co[i]*x**i for i in range(d+1))
+        r=[]
+        r.append(f.subs(x, 0)-1)
+        r.append(f.subs(x, 1)-1)
+        for k in range(1, d):
+            fk=sympy.diff(f, x, k)
+            ff=sympy.diff(f, x, k-1)
+            ll=fk.subs(x, 1)
+            rr=k*ff.subs(x, 0)+fk.subs(x, 0)
+            r.append(ll-rr)
+        p=f.subs(sympy.solve(r, co))
+        return p.subs(x, n)
+d=7 #floating point error ruins higher degree approximations due to chaotic behavior of large matrices, for now 7 is recommended
+print(powerial(0.5, d))
+d=20 #avoids floating point
+print(powerial(sympy.S('1/2'), 8).evalf())
